f5b843aaf30938b4e644fff6fcc5dd48c677d908
Damon Nguyen <DNguyenC@yorksolutions.net>
2025-11-10 15:42:30 -0500

    created spring boot app

---
+++ feedback-analytics-consumer/src/main/java/com/example/feedback_analytics_consumer/FeedbackAnalyticsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedback_analytics_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAnalyticsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAnalyticsConsumerApplication.class, args);
+	}
+
+}
